07d0ca1f325ff9573e9e175589cb900d75c88717
fibo <fibo@users.noreply.github.com>
2014-09-21 17:25:14 +0000

    Squashed 'algebra/' content from commit a98466a
    git-subtree-dir: algebra
    git-subtree-split: a98466a806a608f7a38b4dde0fdb486c07aa7210

---
+++ test/examples.js
@@ -0,0 +1,11 @@
+
+// TODO require('.js')('../examples/algebraOverAnyField.js')
+/*
+var test = require('../examples/algebraOverAnyField.js')
+
+for (var example in test)
+  describe(example + ' example', function () {
+    it('works', test[example])
+  })
+*/
+
