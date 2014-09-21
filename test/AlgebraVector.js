e7ccded53a671e4705f0f23507cda44020e6b04c
fibo <casati_gianluca@yahoo.it>
2014-09-21 17:25:14 +0000

    Squashed 'algebra/' content from commit a98466a
    git-subtree-dir: algebra
    git-subtree-split: a98466a806a608f7a38b4dde0fdb486c07aa7210

---
+++ test/AlgebraVector.js
@@ -0,0 +1,51 @@
+var algebra = require('../index')
+  , should  = require('should')
+
+var AlgebraVector   = algebra.AlgebraVector
+  , RealElement     = algebra.RealElement
+  , RealField       = algebra.RealField
+  , RealVectorSpace = algebra.RealVectorSpace
+
+var one      = new RealElement(1)
+  , minusOne = new RealElement(-1)
+  , two      = new RealElement(2)
+  , zero     = new RealElement(0)
+
+var elements
+  , space
+  , vector
+
+var R  = algebra.R
+  , R3 = new RealVectorSpace(3)
+
+describe('AlgebraVector', function() {
+  describe('Constructor', function() {
+    it('has signature (space, elements)', function() {
+      elements = [one, two]
+      vector = new AlgebraVector(R3, elements)
+
+      vector.should.be.instanceOf(AlgebraVector)
+    })
+  })
+
+  describe('dimension', function() {
+    it('is equal space dimension', function() {
+      elements = [one, zero, minusOne]
+      space    = R3
+      vector = new AlgebraVector(space, elements)
+
+      vector.dimension.should.be.eql(space.dimension)
+    })
+  })
+
+  describe('valueOf()', function() {
+    it('returns element data', function() {
+      elements = [one, zero, minusOne]
+      space    = R3
+      vector = new AlgebraVector(space, elements)
+
+      vector.valueOf().should.be.eql([1, 0, -1])
+    })
+  })
+})
+
