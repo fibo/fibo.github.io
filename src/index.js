314b9ba533142220fda0b94d49de3b1d564b8f8e
fibo <fibo@users.noreply.github.com>
2017-05-30 22:23:53 +0200

    fixing css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function toggleNav () {
   document.querySelector('nav ul').classList.toggle('responsive')
   document.getElementById('hamburgericon').classList.toggle('open')
+  document.querySelector('main').classList.toggle('shifted')
 }
 
 window.toggleNav = toggleNav
