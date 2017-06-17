6876f3ae087b47dffadf88d8a597d096a2ceabc5
fibo <casati_gianluca@yahoo.it>
2017-06-17 15:32:06 +0200

    fix scrolling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ function toggleNav () {
   document.querySelector('nav ul').classList.toggle('responsive')
   document.getElementById('hamburgericon').classList.toggle('open')
   document.querySelector('main').classList.toggle('shifted')
+
+  // Disable scrolling when menu is open.
+
+  var overflow = document.body.overflow
+
+  if (overflow === '') {
+    document.body.overflow = 'hidden'
+  } else {
+    document.body.overflow = ''
+  }
 }
 
 window.toggleNav = toggleNav
