886bc15b8ca882a6f76e146ad5f100c8096207ed
fibo <fibo@users.noreply.github.com>
2017-11-01 17:13:44 +0100

    improved menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function toggleNav () {
+  document.querySelector('nav').style.display = 'initial'
   document.querySelector('nav ul').classList.toggle('responsive')
   document.getElementById('hamburgericon').classList.toggle('open')
   document.querySelector('main').classList.toggle('shifted')
@@ -15,3 +16,30 @@ function toggleNav () {
 }
 
 window.toggleNav = toggleNav
+
+function scrollHandler () {
+  var scrollY = window.pageYOffset || document.documentElement.scrollTop
+  var brake = 10
+  var nav = document.querySelector('nav')
+  navIsHidden = false
+
+  window.addEventListener('scroll', function () {
+    var windowScrollY = window.scrollY
+    var up = scrollY > windowScrollY + brake
+    var down = scrollY < windowScrollY - brake
+
+    if (up && navIsHidden) {
+      nav.style.display = 'initial'
+      navIsHidden = false
+    }
+
+    if (down && !navIsHidden) {
+      nav.style.display = 'none'
+      navIsHidden = true
+    }
+
+    scrollY = windowScrollY
+  }, true)
+}
+
+scrollHandler()
