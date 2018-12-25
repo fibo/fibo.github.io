e42cef6153612b71ebb3d8ec44956278e592244e
fibo <fibo@users.noreply.github.com>
2018-12-25 15:10:37 +0100

    using load-html to load footer

---
+++ src/toggleNav.js
@@ -0,0 +1,18 @@
+function toggleNav () {
+  document.querySelector('nav').style.display = 'initial'
+  document.querySelector('nav ul').classList.toggle('responsive')
+  document.getElementById('hamburgericon').classList.toggle('open')
+  document.querySelector('main').classList.toggle('shifted')
+
+  // Disable scrolling when menu is open.
+
+  var overflow = document.body.style.overflow
+
+  if (overflow === '') {
+    document.body.style.overflow = 'hidden'
+  } else {
+    document.body.style.overflow = ''
+  }
+}
+
+module.exports = toggleNav
