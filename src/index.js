e349afc98230392b2429408cee676fc262cf4a72
Gianluca Casati <fibo@users.noreply.github.com>
2025-07-23 11:25:05 +0700

    more cleanup

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('load-html')
-window.toggleNav = require('./toggleNav')
-var scrollHandler = require('./scrollHandler')
-
-window.addEventListener('load', function() {
-  scrollHandler()
-  loadHtml()
-})
